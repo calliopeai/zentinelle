1cbef976c9f181023c56ec5c66f258a7df044605
Leo Mata <leo@calliope.ai>
2026-09-22 20:34:16 -0600

    style(operator): isort operator view imports (#379)

--- backend/zentinelle/api/views/operator.py
+++ backend/zentinelle/api/views/operator.py
@@ -12,7 +12,6 @@ from rest_framework.views import APIView
 from zentinelle.api.auth import ZentinellePlatformKeyAuthentication
 from zentinelle.models import AgentEndpoint, Policy
 
-
 MAX_AGENT_KEY_TTL_SECONDS = 30 * 24 * 3600
 
 
